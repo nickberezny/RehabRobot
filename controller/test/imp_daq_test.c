913fe3f8f7d899458a875e83a07d876c5ccc3fe5
rehab <nickberezny@hotmail.com>
2018-10-31 16:19:03 -0400

    added FT sensor calibration params

--- controller/test/imp_daq_test.c
+++ controller/test/imp_daq_test.c
@@ -5,6 +5,16 @@
 #include <LabJackM.h> //DAQ library
 #include "include/LJM_Utilities.h"
 
+#define GAIN 43
+//44.210619391
+//44.339016117
+//41.552973115
+//42.95348138
+
+#define OFFSET -0.151194 - 0.005199
+
+#define VERTICLE_OFFSET 0.002646
+
 int main(int argc, char* argv[]) {
 	
 	//init
@@ -14,6 +24,7 @@ int main(int argc, char* argv[]) {
 	int err, handle;
 	double value = 0;
 	double * data;
+	double avg = 0;
 
     // Open first found LabJack
     err = LJM_Open(LJM_dtANY, LJM_ctANY, "LJM_idANY", &handle);
@@ -22,9 +33,31 @@ int main(int argc, char* argv[]) {
   
     printf("Connected to LabJack %s = %f\n", NAME, value); 
 
-    LJM_eReadName(handle, "AIN0", data);
+    for(int i = 1; i<20000; i++)
+    {
+    	LJM_eReadName(handle, "AIN0", data);
+    	*data = GAIN*(*data + OFFSET);
+    	if(i > 1) avg = (avg*(i-1) + *data)/i; 
+    	else avg = *data;
 
-    printf("AIN0 Data: %f\n", data);
+    	//printf("Data: %f\n Average: %f\n", *data, avg);
+    	printf("Data: %f\n",*data);
+    	sleep(1);
+    	
+    }
+ 
+ /*
+    while(1)
+    {
+    	//LJM_eWriteName(handle, "DAC0", 2.5);
+    	LJM_eReadName(handle, "AIN0", data);
+    	*data = 44.4*(*data - 0.152); 
+    	printf("AIN0 Data: %f\n", *data);
+    	sleep(1);
+    }
+    */
+    
+    return;
 
 }
 
