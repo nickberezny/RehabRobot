6aac46b9b3d7fbb05c5b52780c16f49690f27bf5
root <nickberezny@hotmail.com>
2019-05-31 16:00:38 -0400

    1

--- controller/imp_main.c
+++ controller/imp_main.c
@@ -765,7 +765,7 @@ void *server(void* d)
 			if(temp_counter%100 == 0) //refresh UI every _ seconds / 500 = every 0.5 seconds
 			{
 				imp_serve = &((struct impStruct*)d)[i];
-				sprintf(sendBuff,"%.2f,%.2f,%.2f,%.2f,%.2f", imp_serve->xk,imp_serve->xdes,imp_serve->vk,imp_serve->vdes,imp_serve->x_ball);
+				sprintf(sendBuff,"%.2f,%.2f,%.2f,%.2f,%.2f", imp_serve->xk, imp_serve->xdes,imp_serve->vk,imp_serve->vdes,imp_serve->x_ball);
 				send(connfd, sendBuff, strlen(sendBuff), 0);
 			}
 			
