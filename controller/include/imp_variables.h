f2c5d156e8c031c37ee598f510d4d64f085f57dd
nickberezny <nickberezny@hotmail.com>
2019-06-21 14:36:39 -0400

    1

--- controller/include/imp_variables.h
+++ controller/include/imp_variables.h
@@ -60,7 +60,7 @@
 //TODO (for physics)
 #define POSITION_REST 20
 #define VELOCITY_REST 20 
-#define PHYSICS_K 1.0 //TODO - set range
+#define PHYSICS_K 1.0 //TODO - set range ***********************************************
 #define PHYSICS_M 0.1 
 #define CHARACTER_RADIUS 25.0 //radius of ball for balance game
 #define INTERACTION_FORCE_MAX 50 //maximum virtual force for the balance game
